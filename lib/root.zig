39e25e06c904675ac9c200e58c59d3d31a041f8b
Mario-S.O <gh_mario.proclaim@slmail.me>
2025-11-14 20:34:02 +0100

    message sending

--- lib/root.zig
+++ lib/root.zig
@@ -1,23 +1,46 @@
-//! By convention, root.zig is the root source file when making a library.
 const std = @import("std");
 
-pub fn bufferedPrint() !void {
-    // Stdout is for the actual output of your application, for example if you
-    // are implementing gzip, then only the compressed bytes should be sent to
-    // stdout, not any debugging messages.
-    var stdout_buffer: [1024]u8 = undefined;
-    var stdout_writer = std.fs.File.stdout().writer(&stdout_buffer);
-    const stdout = &stdout_writer.interface;
+pub const Message = union(enum) {
+    Increment: void,
+    Print: void,
+};
 
-    try stdout.print("Run `zig build test` to run the tests.\n", .{});
+const Inbox = std.ArrayList(Message);
 
-    try stdout.flush(); // Don't forget to flush!
-}
+pub const Actor = struct {
+    allocator: std.mem.Allocator,
+    counter: u32,
+    inbox: Inbox,
 
-pub fn add(a: i32, b: i32) i32 {
-    return a + b;
-}
+    pub fn init(allocator: std.mem.Allocator) Actor {
+        return .{
+            .allocator = allocator,
+            .counter = 0,
+            .inbox = Inbox.empty,
+        };
+    }
 
-test "basic add functionality" {
-    try std.testing.expect(add(3, 7) == 10);
-}
+    pub fn deinit(self: *Actor) void {
+        self.inbox.deinit(self.allocator);
+    }
+
+    pub fn send(self: *Actor, msg: Message) !void {
+        try self.inbox.append(self.allocator, msg);
+    }
+
+    pub fn run(self: *Actor) void {
+        while (self.inbox.items.len > 0) {
+            const msg = self.inbox.orderedRemove(0);
+            self.handle(msg);
+        }
+    }
+
+    fn handle(self: *Actor, msg: Message) void {
+        switch (msg) {
+            .Increment => self.counter += 1,
+            .Print => std.debug.print("This is my counter: {d}\n", .{self.counter}),
+        }
+    }
+};
+
+const Engine = struct {};
--- src/main.zig
+++ src/main.zig
@@ -1,33 +1,17 @@
 const std = @import("std");
 const zorg = @import("zorg");
-
-const Actor = struct{
-    id: u32,
-    mailbox: Mailbox,
-
-}
+const Actor = zorg.Actor;
 
 pub fn main() !void {
-    // Prints to stderr, ignoring potential errors.
-    std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
-    try zorg.bufferedPrint();
-}
+    var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
+    defer arena.deinit();
 
-test "simple test" {
-    const gpa = std.testing.allocator;
-    var list: std.ArrayList(i32) = .empty;
-    defer list.deinit(gpa); // Try commenting this out and see if zig detects the memory leak!
-    try list.append(gpa, 42);
-    try std.testing.expectEqual(@as(i32, 42), list.pop());
-}
+    var counter_actor = Actor.init(arena.allocator());
+    defer counter_actor.deinit();
 
-test "fuzz example" {
-    const Context = struct {
-        fn testOne(context: @This(), input: []const u8) anyerror!void {
-            _ = context;
-            // Try passing `--fuzz` to `zig build test` and see if it manages to fail this test case!
-            try std.testing.expect(!std.mem.eql(u8, "canyoufindme", input));
-        }
-    };
-    try std.testing.fuzz(Context{}, Context.testOne, .{});
+    try counter_actor.send(.Increment);
+    try counter_actor.send(.Increment);
+    try counter_actor.send(.Print);
+
+    counter_actor.run();
 }
